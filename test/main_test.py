2b548161c8fa669076dd98e9600b8374c1561991
smgilman <smgilman@ncsu.edu>
2024-09-15 00:24:14 -0400

    Changed grid_init to not use numpy (numpy option is commented out if that's the way we want to go)

--- test/main_test.py
+++ test/main_test.py
@@ -6,14 +6,16 @@ import main as main
 
 class MainTest():
 
-    def test_initialize_grid():
-        grid, subgrids = main.MainClass.grid_init()
-        assert grid.size == 10000, f"Total grid size is {grid.size}."
-        assert len(grid) == 100, f"Total grid row length is {len(grid)}."
-        assert len(subgrids) == 20, f"Each subgrid has a row length of {len(subgrids)}."
-        assert all(len(row) == 100 for row in grid), f"Total grid column length is {(len(row) for row in grid)}."
-        assert all(len(row) == 20 for row in subgrids), f"Each subgrid has a column length of {(len(row) for row in subgrids)}."
-        assert (cell == 0 for row in subgrids for cell in row), f"The default value is incorrect."
+    def test_grid_init():
+        subgrids = main.MainClass.grid_init()
 
-    test_initialize_grid()
+        assert len(subgrids) == 400, f"Each subgrid has a row length of {len(subgrids)}."
+        assert all(len(row) == 5 for row in subgrids), f"Each subgrid has a column length of {len(subgrids[0])}."
+        assert (cell == 0 for row in subgrids for cell in row), f"The default value is incorrect."
+        # assert grid.size == 10000, f"Total grid size is {grid.size}."
+        # assert len(grid) == 100, f"Total grid row length is {len(grid)}."
+        # assert all(len(row) == 100 for row in grid), f"Total grid column length is {(len(row) for row in grid)}."
+        
+
+    test_grid_init()
     print("grid_init function passed all tests.")
