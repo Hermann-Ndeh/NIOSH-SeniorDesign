9045efca5ace3054826d9a0cba60a8908bccbe6e
smgilman <smgilman@ncsu.edu>
2024-09-16 08:47:54 -0400

    Reverting back to numpy usage.

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from optimal_number_of_drones import OptimalNumberOfDrones
 from optimal_drone_path import OptimalPath
 from gsd_calculation import GSD
 
-# import numpy as np
+import numpy as np
 
 class MainClass:
     def __init__(self):
@@ -11,21 +11,9 @@ class MainClass:
         self.gsd_calculation = GSD()
 
     def grid_init():
-        grid_size = 100
-        grid = [[0 for _ in range(grid_size)] for _ in range(grid_size)]
-
-        grid_size = len(grid)
-        subgrids = []
-
-        for i in range(0, grid_size, 5):
-            for j in range(0, grid_size, 5):
-                subgrid = [row[j:j + 5] for row in grid[i:i + 5]]
-                subgrids.append(subgrid)
-
-        return subgrids
-        # grid = np.zeros((100, 100), dtype=int)
-        # subgrids = grid.reshape(20, 5, 20, 5).swapaxes(1, 2).reshape(20, 20, 5, 5)
-        # return grid, subgrids
+        grid = np.zeros((100, 100), dtype=int)
+        subgrids = grid.reshape(20, 5, 20, 5).swapaxes(1, 2).reshape(20, 20, 5, 5)
+        return grid, subgrids
 
 
 if __name__ == "__main__":
