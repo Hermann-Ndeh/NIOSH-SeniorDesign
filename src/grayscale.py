7e26f71b56f754866f86d140f66ba7a1845d6f27
smgilman <smgilman@ncsu.edu>
2024-09-29 15:36:50 -0400

    Separating hazards

--- src/grayscale.py
+++ src/grayscale.py
@@ -10,6 +10,7 @@ class DefineGrayScale:
         self.image_side = int(np.sqrt(self.num_grids)) * self.grid_size
         self.image_array = np.zeros((self.image_side, self.image_side), dtype=np.uint8)
         self.grid_values = np.zeros((int(self.image_side / self.grid_size), int(self.image_side / self.grid_size)), dtype=np.uint8)
+        self.red_coordinates = []
         
     def generate_grayscale(self) -> Any:
         for i in range(0, self.image_side, self.grid_size):
@@ -45,3 +46,23 @@ class DefineGrayScale:
 
         # result_image.show()
         result_image.save('img/colored_grids_image.png')
+        self.separate_red_pixels(image_rgb_array)
+
+
+    def separate_red_pixels(self, image_rgb_array):
+        white_image_array = np.full(image_rgb_array.shape, 255, dtype=np.uint8)
+
+        for i in range(0, self.image_side, self.grid_size):
+            for j in range(0, self.image_side, self.grid_size):
+                grid_value = self.image_array[i, j]
+
+                if 0 <= grid_value <= 64:
+                    white_image_array[i:i + self.grid_size, j:j + self.grid_size] = [255, 0, 0]
+                    self.red_coordinates.append((j, i))
+
+        result_image = Image.fromarray(white_image_array)
+
+        result_image.save('img/red_separated_image.png')
+
+        print("Red pixel coordinates:")
+        print(self.red_coordinates)
