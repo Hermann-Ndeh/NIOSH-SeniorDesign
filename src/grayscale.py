cc63328095736f73410bc43fddabae65ad2e8b92
smgilman <smgilman@ncsu.edu>
2024-09-29 16:47:25 -0400

    connecting nodes

--- src/grayscale.py
+++ src/grayscale.py
@@ -1,7 +1,9 @@
 from typing import Any
 import numpy as np
 from PIL import Image
-
+from PIL import ImageDraw
+import networkx as nx
+import math
 
 class DefineGrayScale:
     def __init__(self, grid_size=5, num_grids=400):
@@ -48,7 +50,6 @@ class DefineGrayScale:
         result_image.save('img/colored_grids_image.png')
         self.separate_red_pixels(image_rgb_array)
 
-
     def separate_red_pixels(self, image_rgb_array):
         white_image_array = np.full(image_rgb_array.shape, 255, dtype=np.uint8)
 
@@ -66,3 +67,40 @@ class DefineGrayScale:
 
         print("Red pixel coordinates:")
         print(self.red_coordinates)
+
+        self.connect_isolated_red_pixels(image_rgb_array)
+
+    def connect_isolated_red_pixels(self, image_rgb_array):
+        red_edge_positions = {}
+
+        for x, y in self.red_coordinates:
+            top_edge_center = (x + self.grid_size // 2, y)
+            bottom_edge_center = (x + self.grid_size // 2, y + self.grid_size)
+            left_edge_center = (x, y + self.grid_size // 2)
+            right_edge_center = (x + self.grid_size, y + self.grid_size // 2)
+
+            red_edge_positions[(x, y)] = [top_edge_center, bottom_edge_center, left_edge_center, right_edge_center]
+
+        result_image = Image.fromarray(image_rgb_array)
+        draw = ImageDraw.Draw(result_image)
+
+        for (x1, y1), edges1 in red_edge_positions.items():
+            closest_distance = float('inf')
+            closest_edge1 = None
+            closest_edge2 = None
+            closest_node = None
+            
+            for (x2, y2), edges2 in red_edge_positions.items():
+                if (x1, y1) != (x2, y2):
+                    for edge1 in edges1:
+                        for edge2 in edges2:
+                            distance = math.sqrt((edge1[0] - edge2[0]) ** 2 + (edge1[1] - edge2[1]) ** 2)
+                            if distance < closest_distance:
+                                closest_distance = distance
+                                closest_edge1, closest_edge2 = edge1, edge2
+                                closest_node = (x2, y2)
+
+            if closest_node:
+                draw.line([closest_edge1, closest_edge2], fill=(0, 0, 255), width=1)
+
+        result_image.save('img/connected_red_pixels.png')
